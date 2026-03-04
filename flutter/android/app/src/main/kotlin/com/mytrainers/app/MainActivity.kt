761ba2c3c5dc85199a7cc584951049c16ab67a1c
gaelgael5 <gaelgael5@gmail.com>
2026-03-04 22:48:44 +0100

    Change Flutter package name from com.mytrainers.app.mytrainers_app to com.mytrainers.app
    - Update namespace and applicationId in build.gradle
    - Move MainActivity.kt to new package structure
    - Update package declaration in MainActivity.kt

---
+++ flutter/android/app/src/main/kotlin/com/mytrainers/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mytrainers.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- flutter/android/app/src/main/kotlin/com/mytrainers/app/mytrainers_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mytrainers.app.mytrainers_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
